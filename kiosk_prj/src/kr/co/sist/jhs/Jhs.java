5089401fdd171daf24ad0c2d0db442db6c4fc32c
Godhyunsuk <wngustjr1306@naver.com>
2025-03-31 12:46:14 +0900

    아아아아ㅏ앙

--- kiosk_prj/src/kr/co/sist/jhs/Jhs.java
+++ kiosk_prj/src/kr/co/sist/jhs/Jhs.java
@@ -5,7 +5,7 @@ public class Jhs {
 	public static void main(String[] args) {
 
 		System.out.println("자신이 구현해야할 코드를 작성...");
-		System.out.println("나는 JHS");
+		System.out.println("나는 JHS ");
 	}
 
 }
